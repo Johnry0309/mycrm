26d1e322d4438677214e8c750531181444794f4b
Johnry0309 <johnryqadornado0309@gmail.com>
2026-03-03 20:33:57 +0800

    Link frontend to live PythonAnywhere backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +46,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000", # This is where Next.js runs
     "https://mycrm-omega.vercel.app",
+    "https://mycrm-tau.vercel.app",
+   
     
 ]
 
@@ -127,6 +130,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-import os
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
