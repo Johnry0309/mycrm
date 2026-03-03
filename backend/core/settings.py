045e169e824f4cbfe1376738be7ff6b419e5740e
Johnry0309 <johnryqadornado0309@gmail.com>
2026-03-03 19:58:39 +0800

    latest march commit

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5e&s126w^9#s!^btsetsqs#-(^jn*i*4%&ky@!hf*@tj*n(e6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000", # This is where Next.js runs
+    "https://mycrm-omega.vercel.app",
+    
 ]
 
 MIDDLEWARE = [
